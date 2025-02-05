4e234d8b79e5e99d4bdf4c1cac47e7eedee18a37
aniebietafia <aniebietafia87@gmail.com>
2025-02-05 12:12:08 +0100

    sum_of_digits: handles negative numbers in sum of digits method.

--- src/main/java/com/hng/stageone/numberclassificationapi/util/NumberHelpers.java
+++ src/main/java/com/hng/stageone/numberclassificationapi/util/NumberHelpers.java
@@ -41,6 +41,7 @@ public class NumberHelpers {
 
     public static int sumOfDigits(int number) {
         int sum = 0;
+        number = Math.abs(number);
         while (number != 0) {
             sum += number % 10;
             number /= 10;
